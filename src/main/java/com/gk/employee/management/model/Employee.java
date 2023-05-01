e0355e3ea8fcb50cec6fedaffd46912d8b8a82db
GovindKurmi <govind187250@gmail.com>
2023-05-01 08:29:14 +0530

    added salary service

--- src/main/java/com/gk/employee/management/model/Employee.java
+++ src/main/java/com/gk/employee/management/model/Employee.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +12,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity(name = "employee")
+@Entity
+@Table(name = "employee")
 public class Employee {
 
 	@Id
--- src/main/java/com/gk/employee/management/model/EmployeeSalary.java
+++ src/main/java/com/gk/employee/management/model/EmployeeSalary.java
@@ -1,17 +1,17 @@
 package com.gk.employee.management.model;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "employeesalary")
 public class EmployeeSalary {
   
   @Id
